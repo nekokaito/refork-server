995ccbe43f1454ac9110936e1e6e4e4b4feb17cb
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-05-12 12:33:11 +0600

    JWT Setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const user = process.env.DB_USERNAME;
@@ -10,7 +11,7 @@ const password = process.env.DB_PASSWORD;
 
 // ------- Middleware ----
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${user}:${password}@refork.r4wiaah.mongodb.net/?retryWrites=true&w=majority&appName=ReFork`;
@@ -31,7 +32,13 @@ async function run() {
 
    const foodCollection = client.db('ReFork').collection('food');
    
-
+   //Auth
+   
+   app.post('/jwt', async (req, res) =>{
+           const user = req.body;
+           const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'})
+           res.send(token);
+   })
 
 
 
